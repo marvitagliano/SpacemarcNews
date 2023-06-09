c4bca26c6491318a1a4275769d444849d85d7180
marvitagliano <59565800+marvitagliano@users.noreply.github.com>
2023-06-09 16:31:03 +0200

    Update tags.php

--- tags.php
+++ tags.php
@@ -11,7 +11,7 @@
  *  of the License, or (at your option) any later version.
  *****************************************************************/
 
-header('Content-type: text/html; charset=ISO-8859-1');
+header('Content-type: text/html; charset=UTF-8');
 
 //includo i file di configurazione
 require_once (dirname(__FILE__) . '/config.php');
@@ -44,8 +44,8 @@ $rowconf = @mysqli_fetch_array($conf);
 if ( isset($_GET['tag']) && preg_match('/^([.a-zA-Z0-9-_ ]{2,20})$/', $_GET['tag']) ) {
 	
 	$paginazione = TRUE;
-	$get_tag = htmlspecialchars($_GET['tag'], ENT_QUOTES, "ISO-8859-1");
-	$tag_pager = '&amp;tag=' . htmlspecialchars($_GET['tag'], ENT_QUOTES, "ISO-8859-1");
+	$get_tag = htmlspecialchars($_GET['tag'], ENT_QUOTES, "UTF-8");
+	$tag_pager = '&amp;tag=' . htmlspecialchars($_GET['tag'], ENT_QUOTES, "UTF-8");
 	
 	$rec_page = $rowconf['tags_per_page'];
 	$start = (isset($_GET['start'])) ? abs(floor(intval($_GET['start']))) : 0;
@@ -60,28 +60,28 @@ if ( isset($_GET['tag']) && preg_match('/^([.a-zA-Z0-9-_ ]{2,20})$/', $_GET['tag
 			
 		switch ($rowconf['formato_data']) {
 			case 1:
-				$data = strftime("%a %d %b %Y, %H:%M", $row['data_pubb']);
+				$data = date("D j F Y, H:i", $row['data_pubb']);
 			break;
 			case 2:
-				$data = str_replace("ì", "&igrave;", strftime("%A %d %B %Y, %H:%M", $row['data_pubb']));
+				$data = date("l j F Y, H:i", $row['data_pubb']);
 			break;
 			case 3:
-				$data = strftime("%d/%m/%Y, %H:%M", $row['data_pubb']);
+				$data = date("d/m/Y, H:i", $row['data_pubb']);
 			break;
 			case 4:
-				$data = strftime("%d %b %Y, %H:%M", $row['data_pubb']);
+				$data = date("d M Y, H:i", $row['data_pubb']);
 			break;
 			case 5:
-				$data = strftime("%d %B %Y, %H:%M", $row['data_pubb']);
+				$data = date("d F Y, H:i", $row['data_pubb']);
 			break;
 			case 6:
-				$data = strftime("%m/%d/%Y, %I:%M %p", $row['data_pubb']);
+				$data = date("m/d/Y, H:i", $row['data_pubb']);
 			break;
 			case 7:
-				$data = strftime("%B %d, %Y %I:%M %p", $row['data_pubb']);
+				$data = date("F d, Y H:i", $row['data_pubb']);
 			break;
 			case 8:
-				$data = strftime("%I:%M %p %B %d, %Y", $row['data_pubb']);
+				$data = date("H:i F d, Y", $row['data_pubb']);
 			break;
 		}
 
